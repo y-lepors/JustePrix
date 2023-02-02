11bf89754bf8cd1e7477b189e6e65f0f2014ca5f
lcouedor <leo.couedor@gmail.com>
2023-02-02 14:26:42 +0100

    add style.css and fix button

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ window.addEventListener("DOMContentLoaded", () => {
         {name: 'tableau', price: 500},
         {name: 'tabouret', price: 100},
         {name: 'lit', price: 1000},
-        {name: 'meuble antique', price: 2000}
+        {name: 'meuble antique', price: 2000},
+        {name: 'humain', price: 1}
     ]
 
     const prixAGuess = obj[selectedObject].price;
@@ -16,13 +17,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const divResult = document.getElementById("result")
     const btnValide = document.getElementById("verif")
 
-    const buttonPhrases = [
-        "peut-être ?", "Laurent ?", "je pense ?", "j'imagine ?", "j'espère ?", ", non ?", "alors ?"
-    ]
-
     function HigherOrLower(price) {
-        // console.log("prix:", price)
-        console.log(prixAGuess > price)
         if (prixAGuess > price) {
             divResult.innerHTML = "C'est plus" // true si supérieur
         } else if (prixAGuess < price) {
@@ -34,48 +29,19 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     function init() {
-        divProduit.innerHTML = "Essayons de deviner le prix de ce charmant "
-        divProduit.innerHTML += obj[selectedObject].name
-        divProduit.innerHTML += " !"
-        btnValide.innerHTML = buttonPhrases[Math.floor(Math.random() * buttonPhrases.length)]
+        divProduit.innerHTML = "Essayons de deviner le prix de ce charmant " + obj[selectedObject].name + " !"
+        for(let i = 0; i < obj.length; i++) {
+            let div = document.createElement("div");
+            div.innerHTML = obj[i].name;
+            document.getElementById("roue").appendChild(div);
+        }
     }
 
     function Guess() {
-        let price = inputBox.value
-        UpdateColorInput(price)
-        HigherOrLower(price)
-    }
-
-    function UpdateColorInput(price) {
-        let decalage = Math.abs(1 - Math.abs(price / prixAGuess))
-        console.log("decalage:", decalage)
-        let couleur
-        switch (true) {
-            case decalage < 0.05:
-                couleur = "green"
-                break
-            case decalage < 0.1:
-                couleur = "lightgreen"
-                break
-            case decalage < 0.25:
-                couleur = "yellow"
-                break
-            case decalage < 0.5:
-                couleur = "orange"
-                break
-            default:
-                couleur = "red"
-                break
-        }
-        inputBox.style.borderColor = couleur
-        btnValide.innerHTML = buttonPhrases[Math.floor(Math.random() * buttonPhrases.length)]
+        HigherOrLower(inputBox.value)
     }
 
     function enterGuess() {
-        inputBox.classList.add("animationZoom")
-        setTimeout(() => {
-            inputBox.classList.remove("animationZoom")
-        }, 1000)
         Guess()
         inputBox.placeholder = inputBox.value
         inputBox.value = ""
